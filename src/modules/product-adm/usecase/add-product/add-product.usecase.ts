d5dec8bfdda1c3b941d585a82df664eeffe1507d
Davi Ghiggino <davighiggino3@gmail.com>
2026-02-21 14:50:18 -0300

    feat: implementando dominio de product com testes

---
+++ src/modules/product-adm/usecase/add-product/add-product.usecase.ts
@@ -0,0 +1,29 @@
+import Id from "../../../shared/domain/value-object/id.value-object";
+import ProductGateway from "../../gateway/product.gateway";
+import { Product } from "./../../domain/product.entity";
+import { AddProductInputDto, AddProductOutputDto } from "./add-product.dto";
+
+export default class AddProductUseCase {
+  constructor(private _productRepositoruy: ProductGateway) {}
+  async execute(input: AddProductInputDto): Promise<AddProductOutputDto> {
+    const props = {
+      id: new Id(input.id),
+      name: input.name,
+      description: input.description,
+      purchasePrice: input.purchasePrice,
+      stock: input.stock,
+    };
+    const product = new Product(props);
+    this._productRepositoruy.add(product);
+
+    return {
+      id: product.id.id,
+      name: product.name,
+      description: product.description,
+      purchasePrice: product.purchasePrice,
+      stock: product.stock,
+      createdAt: product.createdAt,
+      updatedAt: product.updatedAt,
+    };
+  }
+}
